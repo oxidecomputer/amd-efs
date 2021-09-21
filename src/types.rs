e9f6e4e592d33a2e564acd894208d4041b81070c
Danny Milosavljevic <daym@oxidecomputer.com>
2021-09-21 16:57:39 +0200

    types: Rename Error::IoError to Error::Io.

--- src/types.rs
+++ src/types.rs
@@ -1,7 +1,7 @@
 
 #[derive(Debug)]
 pub enum Error {
-    IoError(amd_flash::Error),
+    Io(amd_flash::Error),
     EfsHeaderNotFound,
     PspDirectoryHeaderNotFound,
     BiosDirectoryHeaderNotFound,
@@ -15,6 +15,6 @@ pub type Result<Q> = core::result::Result<Q, Error>;
 
 impl From<amd_flash::Error> for Error {
     fn from(error: amd_flash::Error) -> Error {
-        Error::IoError(error)
+        Error::Io(error)
     }
 }
