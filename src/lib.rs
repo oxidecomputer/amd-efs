4a405ed2de2a82de9714c133e60f2a7ceaa5bcfd
Danny Milosavljevic <daym@oxidecomputer.com>
2021-09-27 21:38:15 +0200

    lib: Export PspDirectory, BiosDirectory.

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,9 @@ mod types;
 pub use types::Result;
 pub use types::Error;
 pub use crate::efs::Efs;
+pub use crate::efs::BiosDirectory;
 pub use crate::efs::ProcessorGeneration;
+pub use crate::efs::PspDirectory;
 pub use ondisk::*;
 
 #[cfg(test)]
