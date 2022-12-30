85ff94f0c89c369da610d82d415dd3c8f69dea88
Danny Milosavljevic <daym@oxidecomputer.com>
2022-12-30 02:55:26 +0100

    Make Directory loading a lot more resilient.
    Fixes <https://github.com/oxidecomputer/amd-efs/issues/47>.
    Fixes <https://github.com/oxidecomputer/amd-efs/issues/42>.

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,7 @@ mod struct_accessors;
 mod types;
 pub use crate::efs::preferred_efh_location;
 pub use crate::efs::BhdDirectory;
+pub use crate::efs::ComboDirectory;
 pub use crate::efs::Efs;
 pub use crate::efs::ProcessorGeneration;
 pub use crate::efs::PspDirectory;
