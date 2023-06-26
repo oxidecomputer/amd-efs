6a0f830b57f0cc326a06273a70449040e16809aa
Danny Milosavljevic <dannym@scratchpost.org>
2023-06-26 23:41:22 +0200

    Improve error reporting when using std.
    Fixes <https://github.com/oxidecomputer/amd-efs/issues/78>.

--- src/types.rs
+++ src/types.rs
@@ -1,19 +1,36 @@
 #[derive(Debug)]
+#[cfg_attr(feature = "std", derive(thiserror::Error))]
 pub enum Error {
+    #[cfg_attr(feature = "std", error("io {0}"))]
     Io(amd_flash::Error),
+    #[cfg_attr(feature = "std", error("efs header not found"))]
     EfsHeaderNotFound,
+    #[cfg_attr(feature = "std", error("efs range check"))]
     EfsRangeCheck,
+    #[cfg_attr(feature = "std", error("psp directory header not found"))]
     PspDirectoryHeaderNotFound,
+    #[cfg_attr(feature = "std", error("bhd directory header not found"))]
     BhdDirectoryHeaderNotFound,
+    #[cfg_attr(feature = "std", error("directory range check"))]
     DirectoryRangeCheck,
+    #[cfg_attr(feature = "std", error("directory payload range check"))]
     DirectoryPayloadRangeCheck,
+    #[cfg_attr(feature = "std", error("marshal"))]
     Marshal,
+    #[cfg_attr(feature = "std", error("overlap"))]
     Overlap,
+    #[cfg_attr(feature = "std", error("duplicate"))]
     Duplicate,
+    #[cfg_attr(feature = "std", error("misaligned"))]
     Misaligned,
+    #[cfg_attr(feature = "std", error("entry type mismatch"))]
     EntryTypeMismatch,
+    #[cfg_attr(feature = "std", error("entry not found"))]
     EntryNotFound,
+    #[cfg_attr(feature = "std", error("directory type mismatch"))]
     DirectoryTypeMismatch,
+    #[cfg_attr(feature = "std", error("spi mode mismatch"))]
+    SpiModeMismatch,
 }
 
 pub type Result<Q> = core::result::Result<Q, Error>;
