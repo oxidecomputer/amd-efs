8ca1c5b17aebd635ff80a9d3db18b329db12f31e
Dan Cross <crossd@gmail.com>
2024-02-21 10:12:07 -0500

    Update src/efs.rs
    Co-authored-by: Eliza Weisman <eliza@elizas.website>

--- src/efs.rs
+++ src/efs.rs
@@ -95,11 +95,13 @@ impl<
             return Err(Error::DirectoryTypeMismatch);
         }
         let directory_address_mode = header.additional_info().address_mode();
-        match directory_address_mode {
+        let directory_type_matches = matches!(directory_address_mode,
             AddressMode::PhysicalAddress
-            | AddressMode::EfsRelativeOffset
-            | AddressMode::DirectoryRelativeOffset => {}
-            _ => return Err(Error::DirectoryTypeMismatch),
+                | AddressMode::EfsRelativeOffset
+                | AddressMode::DirectoryRelativeOffset
+        );
+        if !directory_type_matches {
+             return Err(Error::DirectoryTypeMismatch);
         }
         let mut entries = [Item::default(); MAX_DIRECTORY_ENTRIES];
         let mut cursor = beginning
